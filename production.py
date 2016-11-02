6e123f3838901b2c72c4e2a9e4131ddb1d69b095
JPG <JPG@JPGs-MacBook-Pro.local>
2016-11-02 16:04:34 -0600

    filter

--- production.py
+++ production.py
@@ -1,13 +1,15 @@
 import pandas as pd
+import numpy as np
 import matplotlib.pyplot as plt
+from Savitzky_Golay_Filter import savitzky_golay
 
 
-def plot_production(propnum, df, smoother=7):
+def plot_production(propnum, df, window=31, poly=5):
     y_oil = df[df['PROPNUM'] == propnum]['OIL'].reset_index(drop=True)
     y_gas = df[df['PROPNUM'] == propnum]['GAS'].reset_index(drop=True)
     y_water = df[df['PROPNUM'] == propnum]['WATER'].reset_index(drop=True)
     x = range(len(y_oil))
-    y_oil_smooth = y_oil.rolling(window=smoother, center=False).mean()
+    y_oil_smooth = savitzky_golay(y_oil.values, window, poly)
     fig = plt.figure(figsize=(12,8))
     ax = fig.add_subplot(1,1,1)
     ax.plot(x, y_oil, label='Oil', color='green')
@@ -15,8 +17,8 @@ def plot_production(propnum, df, smoother=7):
     ax.plot(x, y_water, label='Water', color='blue')
     plt.legend()
     ax.plot(x, y_oil_smooth, color='green', linestyle='--')
-    ax.scatter(y_oil_smooth.idxmax(), y_oil_smooth.max(), marker='x', s=200, color='black')
-    ax.annotate('Peak', (y_oil_smooth.idxmax(), y_oil_smooth.max()), xytext=(y_oil_smooth.idxmax() + 5, y_oil_smooth.max() + 500), arrowprops=dict(arrowstyle="->"), size=18)
+    ax.scatter(y_oil_smooth.argmax(), y_oil_smooth.max(), marker='x', s=200, color='black')
+    ax.annotate('Peak', (y_oil_smooth.argmax(), y_oil_smooth.max()), xytext=(y_oil_smooth.argmax() + 5, y_oil_smooth.max() + 500), arrowprops=dict(arrowstyle="->"), size=18)
     ax.set_title(propnum)
     ax.set_xlabel('Days On')
     ax.set_ylabel('Production')
@@ -31,14 +33,14 @@ def get_days_production(col, df1, df2):
     df1['Days_Production'] = pd.Series(days)
     return df1['Days_Production']
 
-def get_peak_production(col, df1, df2, ftype='OIL', smoother=7):
+def get_peak_production(col, df1, df2, ftype='OIL', window=31, poly=5):
     peaks = []
     days_to_peak = []
     for label in df1[col]:
         data = df2[df2[col] == label][ftype].reset_index(drop=True)
-        data_smooth = data.rolling(window=smoother, center=False).mean()
+        data_smooth = savitzky_golay(data.values, window, poly)
         peaks.append(data_smooth.max())
-        days_to_peak.append(data_smooth.idxmax())
+        days_to_peak.append(data_smooth.argmax())
     df_peak = pd.Series(peaks)
     df_days_to_peak = pd.Series(days_to_peak)
     return df_peak, df_days_to_peak
@@ -54,9 +56,10 @@ if __name__ == '__main__':
     df_daily.columns = cols
 
     df['Days_Production'] = get_days_production('PROPNUM', df, df_daily)
-    df['OIL_Peak'], df['OIL_Days_to_Peak'] = get_peak_production('PROPNUM', df, df_daily, smoother=7)
+    df['OIL_Peak'], df['OIL_Days_to_Peak'] = get_peak_production('PROPNUM', df, df_daily)
     df.to_csv('../other/frac_merge_peak.csv', index=False)
 
-    # plot_production('R9EKGTQ2EH', df_daily, smoother=7) # 26 days
-    # plot_production('R7FF76VL8I', df_daily, smoother=7) # 53 days
-    # plot_production('L28IBM1H37', df_daily, smoother=7) # 2459 days
+    # plot_production('R9EKGTQ2EH', df_daily) # 26 days production
+    # plot_production('R7FF76VL8I', df_daily) # 53 days production
+    # plot_production('L28IBM1H37', df_daily) # 2459 days production
+    # plot_production('PAVGTJAB8R', df_daily) # 155 days peak
